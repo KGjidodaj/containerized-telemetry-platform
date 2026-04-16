393d18412607af460fb420e5bb2be8371269f072
Kristian Gjidodaj <kristiangjidodaj@gmail.com>
2026-04-16 18:56:35 +0300

    feat: introduce docker-compose and automated lifecycle scripts (setup/cleanup)

---
+++ cleanup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# sudo privilages
+if command -v sudo ;then
+        sudo_cmd="sudo"
+else
+        sudo_cmd=""
+fi
+
+
+echo "cleaning up the directory"
+
+$sudo_cmd docker compose down
+
+cd ..
+
+$sudo_cmd rm -rf containerized-telemetry-platform
---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# sudo privilages
+if command -v sudo ;then
+	sudo_cmd="sudo"
+else
+	sudo_cmd=""
+fi
+
+
+echo "Setting up privilages for all the files"
+
+$sudo_cmd chmod +x system_audit.sh >/dev/null 2>&1
+
+cd data_seeder
+
+$sudo_cmd chmod +x account_creation.sh password_generator.py username_generator.py >/dev/null 2>&1
+
+echo "setting up .html and .sql files"
+
+./account_creation.sh
+
+#going back to the .yaml file
+cd ..
+
+# calling the composer (docker-compose)
+$sudo_cmd docker-compose up -d --build
+
